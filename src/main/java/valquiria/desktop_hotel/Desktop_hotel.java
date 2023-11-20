3e4f7645c35a6b12d04d03f2a8d0310de02067f8
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-11-19 21:53:42 -0500

    FIRST COMMIT

---
+++ src/main/java/valquiria/desktop_hotel/Desktop_hotel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package valquiria.desktop_hotel;
+
+/**
+ *
+ * @author HP
+ */
+public class Desktop_hotel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
