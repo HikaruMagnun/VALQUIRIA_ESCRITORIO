6fc1f932889d5fa6f5ee609135df781b84a78ed0
JEEP2000 <118952656+JEEP2000@users.noreply.github.com>
2023-11-24 22:45:00 -0500

    avance JH

--- src/main/java/valquiria/desktop_hotel/Desktop_hotel.java
+++ src/main/java/valquiria/desktop_hotel/Desktop_hotel.java
@@ -1,18 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package valquiria.desktop_hotel;
 
-/**
- *
- * @author HP
- */
+import Modelo.Conexion;
+import java.sql.Connection;
+
 public class Desktop_hotel {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //RENOVAR EL CODIGO
+        
+        // Llamando al método conectar() desde la clase Conexion
+        Connection conexion = Conexion.conectar();
+
+        Conexion.cerrarConexion();
     }
-    //comentario nuevo
-    
+    //comentario nuevo   
 }
